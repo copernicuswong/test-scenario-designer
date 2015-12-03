3499258c122e855671903c610797f15958f036ba
Copernicus Wong <copernicus.wong@ullink.com>
2015-12-03 14:38:44 +0800

    [IMP] Add Script Execution and Response Handling

--- src/main/javascript/ultest.js
+++ src/main/javascript/ultest.js
@@ -4,6 +4,61 @@ objectName = "com.ullink.testtools.ultest:name=ShipIt,type=ULTestExternalMBean";
 (function() {
     executeScript = function(scriptPath, content, opts) {
         var j4p = new Jolokia(url);
-        j4p.execute(objectName, "executeScript", scriptPath, content, opts);
+        console.debug(content);
+        j4p.execute(objectName, "executeScript", scriptPath, content, {
+            success : function(response) {
+                onExecuting();
+                createExecutionSession(response);
+            }
+        });
     }
+    
+    var currentSession;
+    
+    createExecutionSession = function(response) {
+        console.log(response)
+        currentSession = response;
+    }
+    
+    getFeed = function() {
+        if (currentSession != null) {
+            var j4p = new Jolokia(url);
+            j4p.execute(objectName, "getOutputFeed", currentSession, {
+            success : function(response) {
+                display(response);
+            }
+        });
+        }
+    }
+    
+    display = function(response) {
+        for (var idx in response) {
+            var item = response[idx];
+            if (item.status === "InProgress") {
+            } else if (item.status === "Complete") {
+                onComplete();
+            } else if (item.status === "Error") {
+                onError();
+            }
+            document.getElementById('output-box').value += '\n' + item.message;
+        }
+    }
+    
+    onExecuting = function() {
+        document.getElementById('status-image').src = '../resources/running.png';
+    }
+    
+    onComplete = function() {
+        document.getElementById('status-image').src = '../resources/running.png';
+        currentSession = null;
+    }
+    
+    onError = function() {
+        document.getElementById('status-image').src = '../resources/running.png';
+        currentSession = null;
+    }
+    
+    window.setInterval(getFeed, 500);
+    
+    
 }());
